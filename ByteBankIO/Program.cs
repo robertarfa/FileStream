b605d555a10f2fee6deeb7c30b13964cfcfe0816
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2023-11-28 18:45:03 -0300

    BytesLidos

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -9,40 +9,50 @@ class Program
         //colocar o arquivo na pasta do executável para poder utilizar o caminho mais simples como abaixo.
 
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1; //bytes são iguais a zero ou maiores que zero, nunca serão -1;
 
-
-
-        //abrir arquivo por partes
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
-        //recuperar os bytes que estão dentro do arquivo
-        //byte[] array => armazena os bytes lidos => buffer
-        //int offset => onde vai iniciar o preenchimento do array
-        //int count => quantas posições quer preencher
-
-        var buffer = new byte[1024]; //1024 é 1kb
-
-        numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-
-        while (numeroDeBytesLidos != 0)
+        //using cria um try catch pra verificar se é nulo, se for nulo vai fazer o dispose
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
+
+            var numeroDeBytesLidos = -1; //bytes são iguais a zero ou maiores que zero, nunca serão -1;
+
+            //recuperar os bytes que estão dentro do arquivo
+            //byte[] array => armazena os bytes lidos => buffer
+            //int offset => onde vai iniciar o preenchimento do array
+            //int count => quantas posições quer preencher
+
+            //abrir arquivo por partes
+            var buffer = new byte[1024]; //1024 é 1kb
+
             numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
 
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+
+                Console.WriteLine($"Bytes lidos: {numeroDeBytesLidos}");
+
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+
+            }
+
+            //Essa função vai avisar para o SO que o arquivo não está mais sendo
+            //utilizado e assim podemos modificar o arquivo fora daqui
+            fluxoDoArquivo.Close();
+
+            Console.ReadLine();
         }
-
-
-        Console.ReadLine();
     }
 
-    static void EscreverBuffer(byte[] buffer)
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
     {
         //Unicode contém os 127 caracteres do ASCII
         //UTF faz a transformação do Unicode pra forma que vemos na máquina
         var utf8 = new UTF8Encoding();
 
-        var texto = utf8.GetString(buffer);
+
+        //bytesLidos Para não repetir os dados depois que acabarem os bytes
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
 
         Console.Write(texto);
 
