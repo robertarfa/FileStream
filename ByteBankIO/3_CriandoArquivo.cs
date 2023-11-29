9954c09abae4afa64cc30916ebdd7c8157e230d5
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2023-11-29 08:18:31 -0300

    MetodoFlush

--- ByteBankIO/3_CriandoArquivo.cs
+++ ByteBankIO/3_CriandoArquivo.cs
@@ -32,4 +32,32 @@ partial class Program
         }
 
     }
+
+    static void TestaEscrita()
+    {
+        var caminhoNovoArquivo = "teste.csv";
+
+
+        //FileMode.CreateNew => Somente vai Criar um arquivo se o arquivo não existir
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var escritor = new StreamWriter(fluxoDeArquivo))
+        {
+            //Pra escrever no hd/ssd demora bastante
+            for (int i = 0; i < 1000000; i++)
+            {
+
+                //WriteLine está enviando um buffer do StreamWriter, por isso a demora
+                escritor.WriteLine($"Linha {i}");
+                escritor.Flush();
+                Console.WriteLine($"Linha {i} foi escrita no arquivo. Tecle enter...");
+                Console.ReadLine();
+            }
+
+            //Método flush não vai usar buffer e vai ser mais rápido
+            //Flush despeja o buffer no Stream, logo após utilizar o writeLine
+
+
+        }
+
+    }
 }
